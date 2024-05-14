6430b95d9792e00ea9d2646528944e626b5ce320
Legend7832 <128260152+Legend7832@users.noreply.github.com>
2024-05-14 10:32:29 -1000

    Update update.js

--- background/update.js
+++ background/update.js
@@ -1,1 +1,10 @@
+// backgroundScript.js
 
+import { BackgroundUpdater } from "./backgroundUpdater.js"; // Adjust the import statement
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "changeBackground") {
+    let updater = new BackgroundUpdater();
+    updater.uploadBackground();
+  }
+});
